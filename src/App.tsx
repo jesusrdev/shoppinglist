0219195185ac13dbefb924112cadbe8d41cd3146
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-24 11:32:26 -0500

    adding a toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { Container } from "@mui/material";
+import { useState } from "react";
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <Container></Container>;
+  return <Container>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">
+          Shopping List
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </Container>;
 }
 
 export default App;
