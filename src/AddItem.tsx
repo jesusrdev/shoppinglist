f5deb6b797e42750047a387e2b4171d45f651a40
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-24 12:03:06 -0500

    adding AddItem component to add new item in the modal form

---
+++ src/AddItem.tsx
@@ -0,0 +1,71 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import { Item } from "./App";
+
+type AddItemProps = {
+  addItem: (item: Item) => void;
+}
+
+export default function AddItem(props: AddItemProps) {
+  const [open, setOpen] = useState(false);
+
+  const [item, setItem] = useState<Item>({
+    product: "",
+    amount: 0,
+  });
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const addItem = () => {
+    props.addItem(item);
+    // Clear the text fields and close modal dialog
+    setItem({
+      product: "",
+      amount: 0,
+    });
+    handleClose();
+  }
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Add Item</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>New Item</DialogTitle>
+        <DialogContent>
+          <TextField
+            value={item.product}
+            onChange={(e) => setItem({ ...item, product: e.target.value })}
+            label="Product"
+            margin="dense"
+            fullWidth
+          />
+          <TextField
+            value={item.amount}
+            type="number"
+            onChange={(e) => setItem({ ...item, amount: e.target.value })}
+            label="Amount"
+            margin="dense"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={addItem}>Add</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
 import { AppBar, Container, Toolbar, Typography } from "@mui/material";
 import "./App.css";
+import AddItem from "./AddItem";
+
+export type Item = {
+  product: string;
+  amount: number;
+}
 
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const addItem = (item: Item) => {
+    setItems([item, ...items]);
+  }
+
   return <Container>
     <AppBar position="static">
       <Toolbar>
@@ -11,6 +23,7 @@ function App() {
         </Typography>
       </Toolbar>
     </AppBar>
+    <AddItem addItem={addItem} />
   </Container>;
 }
 
